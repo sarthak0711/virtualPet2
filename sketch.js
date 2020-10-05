a50bbb9d4baa66ec6a57bf55607912d8c101d268
sarthak0711 <68766980+sarthak0711@users.noreply.github.com>
2020-10-05 15:29:22 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -23,6 +23,11 @@ function setup() {
 
   feed=createButton("Feed the dog")
   feed.position(700,95)
+  //feed.mousePressed(feedDog);
+
+  addFood=createButton("Add Food");
+  addFood.position(800,95);
+  //addFood.mousePressed(addFoods)
   
 }
 
